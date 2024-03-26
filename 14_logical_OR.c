e54fa13c2300e219ab9769084e3a27da5e102c6c
Vishal Waghela <103985192+vishalwaghela02@users.noreply.github.com>
2024-03-26 23:00:56 +0530

    Add files via upload

---
+++ 14_logical_OR.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int iNo1 = 0;
+    int iNo2 = 1;
+    int iNo3 = 2;
+    int iAns;
+
+    iAns = iNo1 && ++iNo2 || iNo3++;
+
+    printf(" iNo1 = %d\n iNo2 = %d\n iNo3 = %d\n iAns = %d\n", iNo1, iNo2, iNo3, iAns);
+
+    return 0;
+}
---
+++ 15_bitwise.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("8 & 14: %d\n", 8 & 14); // 8
+    printf("8 | 14: %d\n", 8 | 14); // 14
+
+    printf("8 << 1: %d\n", 8 << 1); // 16
+    printf("8 >> 1: %d\n", 8 >> 1); // 4
+    printf("20 << 2: %d\n", 20 << 2); // 80
+
+    return 0;
+}
