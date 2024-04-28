19573983a0752dfc0a4eea9853357ea7772739da
Vishal Waghela <103985192+vishalwaghela02@users.noreply.github.com>
2024-04-28 17:59:58 +0530

    Add files via upload

---
+++ 42_definition_same_datatype_same_scope.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int iNo = 10;
+    int iNo = 20; // error
+
+    printf("iNo = %d\n", iNo);
+
+    return 0;
+}
---
+++ 43_definition_same_datatype__different_scope.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int iNo = 10;
+    float fNo = 57.53f; 
+
+    printf("iNo = %d\n", iNo); // 10
+
+    return 0;
+}
