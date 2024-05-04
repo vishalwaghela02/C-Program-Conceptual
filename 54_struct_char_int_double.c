dfe69c67eaccc61c9dc20c3d68d012bbb0fe1244
Vishal Waghela <103985192+vishalwaghela02@users.noreply.github.com>
2024-05-04 18:19:32 +0530

    Add files via upload

---
+++ 54_struct_char_int_double.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct demo
+{
+    char chChar;
+    int iNo;
+    double dNo;
+};
+
+int main(void)
+{
+    struct demo obj;
+
+    printf("obj.chChar = %c\nobj.iNo = %d\nobj.dNo = %lf\n", obj.chChar, obj.iNo, obj.dNo); // G1 G1 G3
+
+    obj.chChar = 'A';
+
+    printf("obj.chChar = %c\nobj.iNo = %d\nobj.dNo = %lf\n", obj.chChar, obj.iNo, obj.dNo); // A G2 G3
+ 
+    obj.iNo = 10;
+
+    printf("obj.chChar = %c\nobj.iNo = %d\nobj.dNo = %lf\n", obj.chChar, obj.iNo, obj.dNo); // A 10 G3
+
+    obj.dNo = 99.99;
+
+    printf("obj.chChar = %c\nobj.iNo = %d\nobj.dNo = %lf\n", obj.chChar, obj.iNo, obj.dNo); // A 10 99.99
+
+    return 0;
+}    
